5a37e75dd402755e0e126c1f215b5a700fadb184
Aman Kumar <ar1075840@gmail.com>
2025-09-15 19:55:43 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = `https://realtime-code-editor-j6w9.onrender.com`;
+const url = `https://realtime-code-editor-run.onrender.com`;
 const interval = 30000;
 
 function reloadWebsite() {
